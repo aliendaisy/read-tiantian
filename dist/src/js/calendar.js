ba04bd2bd0b000dd2d32c5907b2387874cf94262
alien <alien@yulingjiedeMacBook-Pro.local>
2018-08-03 00:05:06 +0800

    定稿

--- dist/src/js/calendar.js
+++ dist/src/js/calendar.js
@@ -11,129 +11,127 @@ var mySwiper = new Swiper('#book-swiper', {
 });
 
 //日历部分
+
+//当前年月
 var DATE = new Date();
 var YEAR = DATE.getFullYear();
 var MONTH = DATE.getMonth() + 1;
-var DAY = DATE.getDate();
 
-var startYear = 2018;
-var startMonth = 7; //可根据实际情况修改
-var swiper_from;
+var startYear = 2018; //起始年
+var startMonth = 8; //起始月
 
-//此处json格式保持不变；
-var msg = [
-    {
-        "value": 2018,
-        "childs": [
-            {"value" : 1},
-            {"value" : 2},
-            {"value" : 3},
-            {"value" : 4},
-            {"value" : 5},
-            {"value" : 6},
-            {"value" : 7},
-            {"value" : 8},
-            {"value" : 9},
-            {"value" : 10},
-            {"value" : 11},
-            {"value" : 12}
-        ]
-    },
-    {
-        "value": 2019,
-        "childs": [
-            {"value" : 1},
-            {"value" : 2},
-            {"value" : 3},
-            {"value" : 4},
-            {"value" : 5},
-            {"value" : 6},
-            {"value" : 7},
-            {"value" : 8},
-            {"value" : 9},
-            {"value" : 10},
-            {"value" : 11},
-            {"value" : 12}
-        ]
-    }
-];
+var targetYear = 2020; //目标年
+var targetMonth = 10;  //目标月 （实际为当前年月，此处是为了调试）
 
-//更新日历数据函数
-function updateDate(year, month, day) {
-    //获取二联动中初始月份位置
-    swiper_from = year === startYear ? startMonth : 1;
-    //更新头部月份
-    var header_month = format('YYYY-MM', new Date(year, month - 1 ,day));
-    header_month = header_month.replace(/,/g,'');
-    $('#month').html(header_month);
+var year_value = targetYear - startYear;
+var month_value = targetMonth - startMonth;
 
-    //append操作
-    for(var i = 1; i <= 12; i ++) {
-        //起始月份之前的日期置灰
-        if(year === startYear && i < startMonth) {
-            $('.calendar-date' + i).addClass('calendar-date-before');
-        }else{
-            $('.calendar-date' + i).removeClass('calendar-date-before');
-        }
-        //获取月份的具体日期
-        var dateArr = getMonthDaysArray(year, i, 1, 0);
+var slide_num = year_value * 12 + month_value + 1;  //总共slide数量
 
-        //把具体日期渲染到页面
-        $.each(dateArr, function() {
-            var template_date;
-            if(this.isThisMonth) {
-                template_date = '<li><p>' + this.dayNum + '</p></li>';
-            }else{
-                //不是本月的日期隐藏
-                template_date = '<li><p class="disable-date">' + this.dayNum + '</p></li>';
+//生成mobileSelect插件需要的年月数组（数组格式已存在的不要变动，可以加入项）
+var date_msg = [];
+function createData() {
+    for(var i = startYear; i <= targetYear; i ++) {
+        var obj = {
+            value: i,
+            childs: []
+        };
+        if(i == startYear) {
+            for(var j = startMonth; j <= 12; j ++) {
+                obj.childs.push({value: j});
             }
-            $('.calendar-date' + i).append(template_date);
-        });
-    }
-
-
-    //这里处理用户每日阅读状态请求，可以通过请求改变每个日期的背景色
-    var badgeBlue = '<span class="badge badge-blue">休息</span>';
-    var badgeRed = '<span class="badge badge-red">免费</span>';
-
-    //假数据
-    $('.calendar-date7').find('li p').eq(0).addClass('spot-grey');
-    $('.calendar-date7').find('li p').eq(1).addClass('spot-blue');
-    $('.calendar-date7').find('li p').eq(2).addClass('spot-yellow');
-    $('.calendar-date7').find('li').eq(0).append(badgeBlue);
-    $('.calendar-date7').find('li').eq(1).append(badgeRed);
-
-    $.ajax({
-        type: 'post',
-        url: '',
-        success: function(res) {
-            //对res里的当前月份日期数组进行处理，在dom中找到对应li
-            //已读对li加入spot-yellow的class
-            //补读对li加入spot-blue的class
-            //未读对li加入spot-grey的class
-            //每个月有补足的日期，所以需要对res里的日期数组处理过再遍历进去
+        }else if(i == targetYear) {
+            for(var j = 1; j <= targetMonth; j ++) {
+                obj.childs.push({value: j});
+            }
+        }else{
+            for(var j = 1; j <= 12; j ++) {
+                obj.childs.push({value: j});
+            }
         }
+        date_msg.push(obj);
+    }
+}
+createData();
+
+console.log(date_msg);
+
+
+//计算需要的slide,以及每个月的日历 进行 append操作
+function appendSlide() {
+    $.each(date_msg, function() {
+        var _ = this;
+        $.each(_.childs, function() {
+            var __ = this;
+            var dateArr = getMonthDaysArray(_.value, __.value, 1, 0);
+
+            //单数月份补0
+            if(('' + __.value).length == 1) {
+                __.value = '0' + __.value;
+            }
+
+            //生成带年月id的slide并append
+            var template =
+                '<ul class="swiper-slide calendar-date" id="' + _.value + '-' + __.value + '"></ul>';
+            $('#calendar-swiper .swiper-wrapper').append(template);
+
+            //把具体日期渲染到页面
+            $.each(dateArr, function() {
+                var template_date;
+                if(this.isThisMonth) {
+                    template_date = '<li><p>' + this.dayNum + '</p></li>';
+                }else{
+                    //不是本月的日期隐藏
+                    template_date = '<li><p class="disable-date">' + this.dayNum + '</p></li>';
+                }
+                $('#' + _.value + '-' + __.value).append(template_date);
+            });
+        });
     });
 }
-updateDate(YEAR, MONTH, DAY); //init日历为当前日期月份
+appendSlide();
+
+/*************************************************************************************/
+//这里处理用户每日阅读状态请求，可以通过请求改变每个日期的背景色
+var badgeBlue = '<span class="badge badge-blue">休息</span>';
+var badgeRed = '<span class="badge badge-red">免费</span>';
+
+//假数据
+$('#2018-08').find('li p').eq(3).addClass('spot-grey');
+$('#2018-08').find('li p').eq(4).addClass('spot-blue');
+$('#2018-08').find('li p').eq(5).addClass('spot-yellow');
+$('#2018-08').find('li').eq(6).append(badgeBlue);
+$('#2018-08').find('li').eq(7).append(badgeRed);
+
+$.ajax({
+    type: 'post',
+    url: '',
+    success: function(res) {
+        //对res里的当前月份日期数组进行处理，在dom中找到对应li
+        //已读:对li加入spot-yellow的class
+        //补读:对li加入spot-blue的class
+        //未读:对li加入spot-grey的class
+        //每个月有隐藏的补足的日期，所以需要对res里的日期数组处理过再遍历进去
+    }
+});
+/*************************************************************************************/
+
 
 //选择月份操作
 var mobileSelect = new MobileSelect({
     trigger: '#month',
     title: '选择月份',
     wheels: [
-        {data: msg}
+        {data: date_msg}
     ],
-    position: [0, swiper_from - 1],
+    position: [date_msg.length - 1, targetMonth - 1],
     callback: function(indexArr, data){
-        //更新年月
-        YEAR = data[0].value;
-        MONTH = data[1].value;
+        var m_value = data[0].value + '-' + data[1].value;
+        $('#month').html(m_value); //顶部赋值
 
-        $('.calendar-date li').remove();
-        updateDate(YEAR, MONTH, 1);
+        var slide_index = $('#' + m_value).index();
 
-        calendar_swiper.slideTo(MONTH - 1); //更新相应的日历显示
+        calendar_swiper.slideTo(slide_index); //更新相应月份的日历显示
     }
 });
 
@@ -141,14 +139,14 @@ var mobileSelect = new MobileSelect({
 var calendar_swiper = new Swiper('#calendar-swiper', {
     autoplay: false,
     autoHeight: true,
-    initialSlide: MONTH - 1,
+    initialSlide: slide_num - 1,
     observer: true,
     observerParents: true,
     on: {
         init: function() {
             //初始化时，高度自适应
-            var swiper_from_month = getweeksInMonth(YEAR, swiper_from);
-            var initHeight = swiper_from_month * .74 + .26;
+            var initMonthWeeks = getweeksInMonth(targetYear, targetMonth);
+            var initHeight = initMonthWeeks * .74 + .26;
 
             $('#calendar-swiper').css({minHeight: initHeight + 'rem'});
         },
@@ -157,22 +155,11 @@ var calendar_swiper = new Swiper('#calendar-swiper', {
             //有可能遇到的只有4周的2月份高度自适应
             $('#calendar-swiper').css({minHeight: 'auto'});
 
-            MONTH = this.activeIndex + 1; //activeIndex为切换结束后当前页的顺序值
-
-            $('.calendar-date li').remove();
-            updateDate(YEAR, MONTH, 1); //更新头部年月显示
-
+            var id = $('#calendar-swiper .swiper-wrapper').find('.swiper-slide-active').attr('id');
+            $('#month').html(id);  //获取相应slide的id，并赋值到头部
         }
     }
 });
-
-// console.log($('#calendar-swiper .swiper-wrapper').find('.swiper-slide-active').index())
-// if($('#calendar-swiper .swiper-wrapper').find('.swiper-slide-active').index() == 11) {
-//
-//     console.log(111)
-//     calendar_swiper.removeSlide(0);
-//     calendar_swiper.appendSlide('<ul class="swiper-slide calendar-date"></ul>')
-// }
 
 
 //指引步骤
